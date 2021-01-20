d83e62c2fb0716f2cc551670dff6a73ffa29ee1b
pavelgrafit <pavelgrafit@mail.ru>
2021-01-20 12:31:54 +0300

    test

--- script.js
+++ script.js
@@ -4,41 +4,34 @@ logo = document.getElementById("logo").style;
 
 audio = document.getElementById("audio");
 
-
-
 window.onclick = function(){
-	if(!context){
-		preparation();	
-	}
-	if(audio.paused){
-		audio.play();
-		loop();	
-	}
-	else{
-		audio.pause();
-	}
-	
+    if(!context){
+        preparation();
+    }
+    if(audio.paused){
+        audio.play();
+        loop();
+    }else{
+        audio.pause();
+    }
 }
 
 function preparation(){
-	context = new AudioContext();
-	analyser = context.createAnalyser();
-	src = context.createMediaElementSource(audio);
-	src.connect(analyser);
-	analyser.connect(context.destination);
-	loop();
-
+    context = new AudioContext();
+    analyser = context.createAnalyser();
+    src = context.createMediaElementSource(audio);
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    loop();
 }
 
 function loop(){
-	if(!audio.paused)
-	{
-		window.requestAnimationFrame(loop);
-	}
-	
-	array = new Uint8Array(analyser.frequencyBinCount);
-	analyser.getByteFrequencyData(array);
+    if(!audio.paused){
+        window.requestAnimationFrame(loop);
+    }
+    array = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(array);
 
-	logo.height = (array[40])+"px";
-	logo.width = (array[40])+"px";
+    logo.minHeight = (array[40])+"px";
+    logo.width =  (array[40])+"px";
 }
