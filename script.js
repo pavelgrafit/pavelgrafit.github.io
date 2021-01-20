761e5a2292fd2fe6338c67c2b7a38ec1061d2b30
pavelgrafit <pavelgrafit@mail.ru>
2021-01-20 19:42:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,12 +35,12 @@ function loop(){
     }
     array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
-    console.log(array[900]);
+    console.log(array[0]);
     console.log("NNNNNNNNNNNNNNNNNNNNNNNNNN");
 
-    if ((array[900]/150 > 1) && (array[900]/150 < 1.4)) 
+    if ((array[0]/150 > 1) && (array[0]/150 < 1.4)) 
     {
-    	logo.transform = "scale(" + (array[900]/150).toString() + ")";
+    	logo.transform = "scale(" + (array[0]/150).toString() + ")";
     }
     else {
     	logo.transform = "scale(1)";
